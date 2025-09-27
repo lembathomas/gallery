b8eb6163be4333dd7566714ae16eb045b4ce010b
lembathomas <lembathomas@gmail.com>
2025-09-27 18:56:02 +0300

    update file

--- server.js
+++ server.js
@@ -12,45 +12,30 @@ let image = require('./routes/image');
 const app = express();
 
 // connecting the database
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env];
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
+        console.log(err);
+    } else {
+        console.log(`Connected to Database: ${MONGODB_URI}`);
     }
 });
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
-
-
 
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Set up the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // body parser middleware
-app.use(express.json())
-
+app.use(express.json());
 
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
 });
 
-}
 module.exports = app;
