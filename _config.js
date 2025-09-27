3b3b51578417ca75203ce492db2b6e76ad896d06
lembathomas <lembathomas@gmail.com>
2025-09-27 20:28:07 +0300

    Added code

--- _config.js
+++ _config.js
@@ -1,15 +1,4 @@
 var config = {}
-
-// Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
-module.exports = config;
-
-var config = {}
-
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://thomas:Uiek1St4911EcQeO@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
@@ -17,3 +6,6 @@ config.mongoURI = {
     test: 'mongodb+srv://thomas:Uiek1St4911EcQeO@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
+
+var config = {}
+
