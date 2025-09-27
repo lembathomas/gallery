82524358033bedcb02094a8d19bace84c37e1a36
lembathomas <lembathomas@gmail.com>
2025-09-27 20:33:09 +0300

    Update the coreect connection string

--- _config.js
+++ _config.js
@@ -1,11 +1,10 @@
 var config = {}
+
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://thomas:Uiek1St4911EcQeO@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://thomas:Uiek1St4911EcQeO@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://thomas:Uiek1St4911EcQeO@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://thomas:Uiek1St4911EcQeO@projectdevops.lyuwnqd.mongodb.net/darkroom?retryWrites=true&w=majority&appName=ProjectDevops',
+    development: 'mongodb+srv://thomas:Uiek1St4911EcQeO@projectdevops.lyuwnqd.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=ProjectDevops',
+    test: 'mongodb+srv://thomas:Uiek1St4911EcQeO@projectdevops.lyuwnqd.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=ProjectDevops'
 }
-module.exports = config;
 
-var config = {}
-
+module.exports = config
