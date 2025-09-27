0d8ca7d2f555358123f828bcaf5cf5bbdfaac190
lembathomas <lembathomas@gmail.com>
2025-09-27 20:50:52 +0300

    correct conection string MongoDB

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://thomas:Uiek1St4911EcQeO@projectdevops.lyuwnqd.mongodb.net/darkroom?retryWrites=true&w=majority&appName=ProjectDevops',
-    development: 'mongodb+srv://thomas:Uiek1St4911EcQeO@projectdevops.lyuwnqd.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=ProjectDevops',
-    test: 'mongodb+srv://thomas:Uiek1St4911EcQeO@projectdevops.lyuwnqd.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=ProjectDevops'
+    production: 'mongodb+srv://thomas:I9J0MUsmJmMXKD4F@projectdevops.lyuwnqd.mongodb.net/?retryWrites=true&w=majority&appName=ProjectDevops
+    development: 'mongodb+srv://thomas:I9J0MUsmJmMXKD4F@projectdevops.lyuwnqd.mongodb.net/?retryWrites=true&w=majority&appName=ProjectDevops
+    test: 'mongodb+srv://thomas:I9J0MUsmJmMXKD4F@projectdevops.lyuwnqd.mongodb.net/?retryWrites=true&w=majority&appName=ProjectDevops
 }
 
 module.exports = config
